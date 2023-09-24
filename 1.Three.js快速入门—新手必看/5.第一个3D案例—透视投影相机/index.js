b35f32424e7c25b7995d6fd663200a5fd7866698
Amber-lb <359564897@qq.com>
2023-09-24 22:07:59 +0800

    '透视投影相机'

---
+++ 1.Three.js快速入门—新手必看/5.第一个3D案例—透视投影相机/index.js
@@ -0,0 +1,37 @@
+// 引入threejs
+import * as THREE from 'three'
+/**
+ * 创建3D场景对象Scene
+ */
+const scene = new THREE.Scene()
+/**
+ * 创建网格模型
+ */
+// 创建一个长方体几何对象Geometry
+const geometry = new THREE.BoxGeometry(50, 50, 50)
+// 材质对象Material
+const material = new THREE.MeshBasicMaterial({
+    color: 0x0000ff, // 设置材质颜色
+})
+// 网格模型对象mesh
+const mesh = new THREE.Mesh(geometry, material)
+// 设置网格模型在三维空间中的位置坐标，默认是坐标原点
+mesh.position.set(0, 10, 0)
+// 将网格模型添加到场景中
+scene.add(mesh)
+
+// width和height用来设置渲染后，输出的画布宽高度
+const width = 800 // 宽度
+const height = 500 // 高度
+
+/**
+ * 透视投影相机设置
+ */
+// fov：视场角度，aspect：width / height Canvas画布宽高比，near：近裁截面，far：远裁截面
+const camera = new THREE.PerspectiveCamera(30, width / height, 0.1, 3000)
+// 相机在Three.js三维坐标系中的位置
+camera.position.set(300, 200, 200)
+// 相机观察目标指向Three.js坐标系原点
+camera.lookAt(0, 0, 0)
+
+console.log(camera)
